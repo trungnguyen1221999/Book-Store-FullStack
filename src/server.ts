59c8f374b7e3fb941a24f24a9a6402c2472bd7db
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-18 08:13:10 +0300

    init project

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./routers/web.js";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
